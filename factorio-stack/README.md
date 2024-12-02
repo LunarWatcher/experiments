d361e3f8add756f0183cc7968c9c76438a501d85
Olivia <zoe.i2k1@gmail.com>
2024-12-02 01:05:49 +0100

    UI fun, transform matrices, concluding notes

--- factorio-stack/README.md
+++ factorio-stack/README.md
@@ -1,4 +1,4 @@
-# factorio-stack [ongoing]
+# factorio-stack [ended]
 
 Minor test game based on the same-ish core tech stack as factorio, with SDL2, allegro5, and agui
 
@@ -15,7 +15,41 @@ Based on reports, it seems that the main problems are input-related (shock), and
 
 It does seem that factorio directly interacts with IME (https://forums.factorio.com/116118), so certain input things may still need to be handled in the game, but that's a future me problem that I can manage. Text rendering, on the other hand, is just cancer to sort out, and is _the_ main bit I don't want to work extensively with. Along with standardised input and UI rendering, because these are solved problems, but are also wildly complex when not constraining what's being rendered.
 
-## Results
+> ![NOTE]
+> Corrections:
+> * Allegro doesn't support IME input, so factorio's implementation (that doesn't cooperate with ibus) might be a hack?
+> * Agui has been heavily modified by factorio (and those modifications are closed-source), input in the main version appears to be borked. Further experiments required
+> * Implementing a GUI library from scratch in Allegro doesn't look like it would be the worst thing to do
+
+## Results 
+
+### Allegro in general
 * Including both necessary and unnecessary boilerplate, and various other useless overengineered shit largely based on a tutorial from the allegro wiki, font rendering with full unicode support (given a font that supports the characters) is done in just 74 lines of code. For something that isn't a full game engine, that's pretty damn good. This is also prior to fucking around with agui
+* **Single line image rendering** (not including image loading, which is another 2 lines)
+* **Single line primitive drawing**
+* Complex primitives are also a single call, but obviously requires vertices sorted out manually
+* ✨ Supports shaders ✨, both GLSL and whatever the fuck HLSL is 
+    * I _think_ HLSL is for direct3D. Might need to force OpenGL for it to cooperate. Should probably do that anyway.
 
+AKA _exactly_ what I'm looking for.
 
+### GUI
+* Misc. agui stuff:
+    * The input fields doesn't display the IME thing (at least under Linux), but it accepts at least Korean input correctly. It failed to display the emoji input thing and unicode input thing, which suggests some further input magic is required to properly work with IME
+    * The input fields have mysterious double characters when typing in English (`e` becomes `ee`, `E` becomes `eE`). Based on the source code for agui, it appears that the input management system is out of date and doesn't properly handle `KEY_CHAR` and `KEY_DOWN` events. I'm not sure what has changed since agui was made, however.
+    * Based on [this post](https://forums.factorio.com/viewtopic.php?t=110516) on the factorio forums (referencing `agui::TableWithSelection`, which does not exist), it looks like Factorio has a private fork of `agui`
+        * Also just found [this FFF](https://www.factorio.com/blog/post/fff-216) that doesn't exactly suggest agui is particularly nice. [FFF-212](https://www.factorio.com/blog/post/fff-212) explicitly states that they're using a "heavily modified AGUI"
+    * I suspect that if I go down this rabbithole, I'll need to make a fork of agui
+* Misc. general GUI stuff:
+    * There aren't that many GUI libraries for allegro. 
+        * [lgui](https://github.com/frank256/lgui): like agui, not actively maintained, seems to have overall less support and popularity though
+        * [EagleGUI](https://github.com/EdgarReynaldo/EagleGUI): Actively maintained, but seems to abstract allegro behind itself. Seems more like a GUI library in the same category as Qt or GTK rather than one meant for games. 
+        * Plus various otehrs listed on the wiki that are also dead and not worth mentioning, or is straight up Qt, WxWidgets, or imgui.
+    * Though imgui is not inteded for it, I'm starting to suspect it might be the easiest way to implement at least some parts of the UI for the time being. It's explicitly stated that it supports IME stuff: https://github.com/ocornut/imgui/blob/ee2119d7cbe1361689f9a7a6ef6853c1deae08f6/docs/BACKENDS.md?plain=1#L24
+    * That said, [allegro does not support IME input](https://github.com/liballeg/allegro5/issues/1191), but I'll ignore that for now. Too low-level for me to do anything about anyway. 
+
+I overall have a weak spot for forking, because it looks like most of the tedious shit is done, and Allegro alone does a lot of heavy lifting. Allegro has functionally native text rendering support with full unicode and TTF support, so it wouldn't be nearly as difficult. Using agui will require significant work before it's even barely usable, which isn't great. On the other hand, doing so might be more useful. 
+
+Looks like the shortest path to a GUI is imgui, while the [!!FUN!!](https://dwarffortresswiki.org/index.php/Fun) path is forking agui.
+
+(Why do I do this to myself?)
--- factorio-stack/src/factorio-stack/Main.cpp
+++ factorio-stack/src/factorio-stack/Main.cpp
@@ -1,3 +1,4 @@
+#include "allegro5/allegro_image.h"
 #include "allegro5/allegro_primitives.h"
 #include <iostream>
 
@@ -9,7 +10,21 @@
 #include <Agui/Agui.hpp>
 #include <Agui/Backends/Allegro5/Allegro5.hpp>
 #include <Agui/Widgets/Button/Button.hpp>
+#include "Agui/Widgets/TextField/TextField.hpp"
 #include <Agui/FlowLayout.hpp>
+#include "Agui/Color.hpp"
+#include "allegro5/bitmap.h"
+#include "allegro5/events.h"
+#include "allegro5/keycodes.h"
+#include "allegro5/transformations.h"
+
+
+class ALTest : public agui::ActionListener {
+public:
+    virtual void actionPerformed(const agui::ActionEvent &evt) {
+        std::cout << "I can has been clicked" << std::endl;
+    }
+};
 
 int main() {
     constexpr auto CJK_FONT = "/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc";
@@ -23,8 +38,13 @@ int main() {
     al_init_font_addon();
     al_init_ttf_addon();
     al_init_primitives_addon();
+    al_init_image_addon();
+    //Borked for some reason (config error?)
+    //al_init_native_dialog_addon();
     al_install_keyboard();
+    al_install_mouse();
 
+    ALLEGRO_BITMAP* bmp = al_load_bitmap("../test.png");
 
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
 
@@ -33,13 +53,15 @@ int main() {
 
     ALLEGRO_DISPLAY* disp = al_create_display(640, 480);
     al_set_window_title(disp, "Choo choo");
+    al_set_display_icon(disp, bmp);
 
     ALLEGRO_FONT* font = al_load_ttf_font(CJK_FONT, 36, 0);
 
 
     al_register_event_source(queue, al_get_keyboard_event_source());
-    al_register_event_source(queue, al_get_display_event_source(disp));
+    al_register_event_source(queue, al_get_mouse_event_source());
     al_register_event_source(queue, al_get_timer_event_source(timer));
+    al_register_event_source(queue, al_get_display_event_source(disp));
 
     agui::Gui* gui;
     agui::Allegro5Input* inputHandler;
@@ -54,6 +76,8 @@ int main() {
     graphicsHandler = new agui::Allegro5Graphics();
     gui = new agui::Gui();
 
+    ALTest evl;
+
     gui->setInput(inputHandler);
     gui->setGraphics(graphicsHandler);
     auto uiFont = agui::Font::load(CJK_FONT, 16);
@@ -61,20 +85,57 @@ int main() {
 
     agui::FlowLayout flow;
     gui->add(&flow);
-    flow.setMargins(200, 0, 0, 0);
+    flow.setMargins(0, 0, 0, 0);
+    flow.setLocation(150, 150);
 
     agui::Button btn;
     flow.add(&btn);
     btn.setSize(400, 30);
     btn.setText("Push me uwu æøå ㄴㅇㄴㅁㄹㅇㄴ엉");
+    btn.setBackColor(agui::Color(1.0f, 0.0f, 1.0f));
+    btn.setToolTipText("Test");
+    btn.addActionListener(&evl);
+
+    agui::TextField tf;
+    flow.add(&tf);
+    tf.setBackColor(agui::Color(0.5f, 0.5f, 0.5f));
+    tf.setFontColor(agui::Color(0.9f, 0.9f, 0.9f));
+    tf.setSize(400, 30);
+    tf.addActionListener(&evl);
+    //Action seems to be submit or completed, while other events exist for other things
+    //tf.addKeyboardListener(KeyboardListener *listener)
 
     // Bit annoying that there's now two ways to init the font, but looks like there's no way around it.
     // With imgui in the mix for debugging stuff, that's potentially three separate ways to load fonts (not sure how
     // imgui works, future me problem)
+    
+    ALLEGRO_TRANSFORM worldCam, uiCam;
+
+    al_identity_transform(&worldCam);
+    al_orthographic_transform(
+        &worldCam,
+        0.0f, 0.0f,
+        -1.0f,
+        640.0f, 480.0f, 
+        1.0f
+    );
+
+    al_identity_transform(&uiCam);
+    al_orthographic_transform(
+        &uiCam,
+        0.0f, 0.0f,
+        -1.0f,
+        640.0f, 480.0f, 
+        1.0f
+    );
+    
+
 
     bool done = false;
     bool redraw = true;
     ALLEGRO_EVENT event;
+
+    float x = 0, y = 0;
 
     al_start_timer(timer);
     while(1) {
@@ -88,6 +149,22 @@ int main() {
             case ALLEGRO_EVENT_DISPLAY_CLOSE:
                 done = true;
                 break;
+            case ALLEGRO_EVENT_DISPLAY_RESIZE:
+                al_acknowledge_resize(event.display.source);
+                gui->resizeToDisplay();
+                break;
+            case ALLEGRO_EVENT_DISPLAY_SWITCH_IN:
+                gui->resizeToDisplay();
+                break;
+            case ALLEGRO_EVENT_KEY_DOWN:
+                if (event.keyboard.keycode == ALLEGRO_KEY_A) {
+                    x -= 1;
+                    al_translate_transform(&worldCam, -1, 0);
+                } else if (event.keyboard.keycode == ALLEGRO_KEY_D) {
+                    x -= 1;
+                    al_translate_transform(&worldCam, 1, 0);
+                }
+                break;
         }
 
         inputHandler->processEvent(event);
@@ -96,14 +173,17 @@ int main() {
             break;
 
         if(redraw && al_is_event_queue_empty(queue)) {
+            gui->logic();
 
             al_clear_to_color(al_map_rgb(0, 0, 0));
+
+            al_use_projection_transform(&worldCam);
             al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world! 여오");
 
-            if (gui) {
-                gui->logic();
-                gui->render();
-            }
+            al_draw_bitmap(bmp, 100, 100, 0);
+
+            al_use_projection_transform(&uiCam);
+            gui->render();
 
             al_flip_display();
 
@@ -111,6 +191,8 @@ int main() {
             redraw = false;
         }
     }
+
+    al_destroy_bitmap(bmp);
 
     al_destroy_font(font);
     al_destroy_display(disp);
